b221530ee1f9dbdf478ce12bc2712d9c80749780
ngarikev <kevin.gary191@gmail.com>
2025-08-18 19:27:38 +0300

    updated

--- src/data/properties.ts
+++ src/data/properties.ts
@@ -14,10 +14,30 @@ import property13 from "@/assets/3br-property13.webp";
 import property14 from "@/assets/3br-property14.webp";
 import property15 from "@/assets/3br-property15.webp";
 
+//2br images
+import twobrproperty1 from "@/assets/2br-property1.webp";
+import twobrproperty2 from "@/assets/2br-property2.webp";
+import twobrproperty3 from "@/assets/2br-property3.webp";
+import twobrproperty4 from "@/assets/2br-property4.webp";
+import twobrproperty5 from "@/assets/2br-property5.webp";
+import twobrproperty6 from "@/assets/2br-property6.webp";
+import twobrproperty7 from "@/assets/2br-property7.webp";
+import twobrproperty8 from "@/assets/2br-property8.webp";
+import twobrproperty9 from "@/assets/2br-property9.webp";
+import twobrproperty10 from "@/assets/2br-property10.webp";
+import twobrproperty11 from "@/assets/2br-property11.webp";
+import twobrproperty12 from "@/assets/2br-property12.webp";
+import twobrproperty13 from "@/assets/2br-property13.webp";
+import twobrproperty14 from "@/assets/2br-property14.webp";
+import twobrproperty15 from "@/assets/2br-property15.webp";
+import twobrproperty16 from "@/assets/2br-property16.webp";
+import twobrproperty17 from "@/assets/2br-property17.webp";
+
+
 export const properties = [
   {
     id: "1",
-    name: "3br apartment",
+    name: "Three bedroom apartment",
     location: "Nyali, Mombasa",
     description: "Luxury apartment with panoramic ocean views...",
     images: [property1, property2, property4, property5, property6, property7, property8, property9, property10, property11, property12, property13, property14, property15], // Add more images here
@@ -32,15 +52,15 @@ export const properties = [
   },
   {
     id: "2",
-    name: "Coastal Retreat",
-    location: "Zanzibar, Tanzania",
+    name: "Two bedroom apartment",
+    location: "Nyali, Mombasa",
     description: "Stunning beachfront property...",
-    images: [property2, property1, property3],
+    images: [twobrproperty1, property1, twobrproperty2, twobrproperty3, twobrproperty4, twobrproperty5, twobrproperty6, twobrproperty7, twobrproperty8, twobrproperty9, twobrproperty10, twobrproperty11, twobrproperty12, twobrproperty13, twobrproperty14, twobrproperty15, twobrproperty16, twobrproperty17],
     price: "$110",
     guests: 4,
     bedrooms: 2,
     bathrooms: 2,
-    amenities: ["Wi-Fi", "Beach Access", "Snorkeling", "Parking", "AC", "Kitchen"],
+    amenities: ["Wi-Fi", "Beach Access", "Swimming pool", "Parking", "AC", "Kitchen"],
     rating: 4.8,
     reviews: 89,
     featured: false
